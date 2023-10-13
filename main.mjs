473fa1825ff8e849947d8be3297c6c32cf661f38
MGutuNikhil <gurunikhilmangaraju@gmail.com>
2023-10-13 21:26:05 +0530

    okay

--- main.mjs
+++ main.mjs
@@ -1,16 +1,23 @@
 import sha256 from 'crypto-js/sha256.js';
 
+class Transaction {
+    constructor(fromAccount,toAccount,amount) {
+        this.fromAccount = fromAccount;
+        this.toAccount = toAccount;
+        this.amount = amount;
+    }
+}
+
 class Block {
-    constructor(index,data,previousHash='') {
-        this.index = index;
-        this.data = data;
+    constructor(transactions,previousHash='') {
+        this.transactions = transactions;
         this.nonce = 0;
         this.previousHash = previousHash;
         this.hash = this.generateHash();
     }
 
     generateHash() {
-        return sha256(this.index + this.previousHash + this.nonce + JSON.stringify(this.data)).toString();
+        return sha256(this.index + this.previousHash + this.nonce + JSON.stringify(this.transactions)).toString();
     }
 
     mineBlock(level) {
@@ -26,20 +33,55 @@ class BlockChain {
     constructor() {
         this.chain = [this.createGenesis()];
         this.level = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     createGenesis() {
-        return new Block(0,"Genesis Block","0");
+        return new Block("Genesis Block","0");
     }
 
     getLastBlock() {
         return this.chain[this.chain.length-1];
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLastBlock().hash;
-        newBlock.mineBlock(this.level);
-        this.chain.push(newBlock);
+    // addBlock(newBlock) {
+    //     newBlock.previousHash = this.getLastBlock().hash;
+    //     newBlock.mineBlock(this.level);
+    //     this.chain.push(newBlock);
+    // }
+
+    mPT(miningRewardAccount) {
+        let block = new Block(this.pendingTransactions,this.getLastBlock().hash);
+        block.mineBlock(this.level);
+
+        console.log("Block successfully mined");
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAccount, this.miningReward)
+        ];
+    }
+
+    addTransaction(Transaction) {
+        this.pendingTransactions.push(Transaction);
+    }
+
+    balanceOfAccount(account) {
+        let balance = 0;
+
+        for(const block of this.chain) {
+            for(const transaction of block.transactions) {
+                if(transaction.fromAccount === account) {
+                    balance -= transaction.amount;
+                }
+                if(transaction.toAccount === account) {
+                    balance += transaction.amount;
+                }
+            }
+        }
+
+        return balance;
     }
 
     isValid() {
@@ -60,12 +102,15 @@ class BlockChain {
 }
 
 let nikBlock = new BlockChain();
+nikBlock.addTransaction(new Transaction("Nikhil","Nischal",100));
+nikBlock.addTransaction(new Transaction("Nischal","Nikhil",50));
 
-console.log("Mining block 1...");
-nikBlock.addBlock(new Block(1,"Nikhil"));
+console.log("\nMining...");
+nikBlock.mPT("pp");
 
-console.log("Mining block 2...");
-nikBlock.addBlock(new Block(2,"Nischal"));
+console.log("\nBalance of pp : " + nikBlock.balanceOfAccount("pp"));
 
-console.log(JSON.stringify(nikBlock,null,4));
-console.log(nikBlock.isValid());
+console.log("\nMining...");
+nikBlock.mPT("pp");
+
+console.log("\nBalance of pp : " + nikBlock.balanceOfAccount("pp"));
